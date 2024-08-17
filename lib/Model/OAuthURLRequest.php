676b27323ca39c1f09b97b9dd4a3aa41c88a6736
konfig-publisher <publisher@konfigthis.com>
2024-08-17 00:04:51 +0000

    Regenerate SDKs

--- lib/Model/OAuthURLRequest.php
+++ lib/Model/OAuthURLRequest.php
@@ -1185,7 +1185,7 @@ class OAuthURLRequest implements ModelInterface, ArrayAccess, \JsonSerializable
     /**
      * Sets use_ocr
      *
-     * @param bool|null $use_ocr Enable OCR for files that support it. Supported formats: pdf
+     * @param bool|null $use_ocr Enable OCR for files that support it. Supported formats: jpg, png, pdf
      *
      * @return self
      */
